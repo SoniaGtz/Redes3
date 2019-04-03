069cdab8d6356e1cbee1c8ac6f2cc680b761d0c4
JessRdz <jesus-antonio-29@hotmail.com>
2019-04-03 08:37:05 -0600

    a darle x2

--- SyslogServer.py
+++ SyslogServer.py
@@ -3,10 +3,22 @@ HOST, PORT = "50.0.0.2", 514
 
 import logging
 import socketserver
+from Email import send_notification
 
 logging.basicConfig(level=logging.DEBUG, format='%(message)s', datefmt='', filename=LOG_FILE, filemode='a')
 
+#Obtiene el nivel del mensaje (0 a 7)
+def obtenerNivel(mensaje_syslog):
+    inicio = mensaje_syslog.find("<") + 1
+    fin = mensaje_syslog.find(">")
+    nivel = int(mensaje_syslog[inicio:fin]) % 8
+    return nivel
 
+#Manda notificaciones por correo, sms y notifica por sistema
+def notificar(mensaje_syslog):
+
+
+#Comportamiento del servidor UDP que recibirá los logs
 class SyslogUDPHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
