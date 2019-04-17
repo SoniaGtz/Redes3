3092f6c49e8d4fee610322bf0d8c242a3b61073a
SoniaGtz <sonia_gtz05@hotmail.com>
2019-04-16 23:10:24 -0500

    Traps, preguntas 2 y 4

--- TrapReceiver.py
+++ TrapReceiver.py
@@ -2,6 +2,7 @@ from pysnmp.entity import engine, config
 from pysnmp.carrier.asyncore.dgram import udp
 from pysnmp.entity.rfc3413 import ntfrcv
 from pysnmp.proto.api import v2c
+from Email import send_notification
 
 # Create SNMP engine with autogenernated engineID and pre-bound
 # to socket transport dispatcher
@@ -23,8 +24,21 @@ config.addTransport(
 config.addV3User(
     snmpEngine, 'demo',
     config.usmHMACMD5AuthProtocol, 'password',
-    securityEngineId=v2c.OctetString(hexValue='800000090300C2020BB50000')
+    securityEngineId=v2c.OctetString(hexValue='800000090300CA0153900000')
 )
+
+f = open("OIDs", "r")
+oids = f.readlines()
+
+
+def analize(name, valor):
+
+    for i in oids:
+        oid, desc = i.rstrip('\n').split("|")
+        if name.find(oid) != -1:
+            mensaje = desc + ": " + valor
+            send_notification("sonia_gtz05@hotmail.com","Alerta SNMP", mensaje)
+
 
 # Callback function for receiving notifications
 # noinspection PyUnusedLocal,PyUnusedLocal,PyUnusedLocal
@@ -34,6 +48,7 @@ def cbFun(snmpEngine, stateReference, contextEngineId, contextName,
                                                                         contextName.prettyPrint()))
     for name, val in varBinds:
         print('%s = %s' % (name.prettyPrint(), val.prettyPrint()))
+        analize(str(name.prettyPrint()), str(val.prettyPrint()))
 
 
 # Register SNMP Application at the SNMP engine
